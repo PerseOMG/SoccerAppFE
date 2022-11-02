990546524127768a76a1ede0ec0794ee1c8be0ea
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2022-11-01 19:38:19 -0600

    App Soccer | Creating card full detail page

--- src/app/components/card/card-full/card-full.component.ts
+++ src/app/components/card/card-full/card-full.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamsFacade } from '../../../services/teams/teams.facade';
+import { Team } from '../../../models/team.models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-card-full',
   templateUrl: './card-full.component.html',
-  styleUrls: ['./card-full.component.scss']
+  styleUrls: ['./card-full.component.scss'],
 })
 export class CardFullComponent implements OnInit {
+  team$ = this.teamsFacade.getTeamSelected();
+  constructor(private teamsFacade: TeamsFacade) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
