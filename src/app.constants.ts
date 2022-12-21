ea4cce9089e2988a4382e76c4c0ecae2e1889791
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2022-12-20 20:13:57 -0600

    App Soccer | Back to local server url

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,3 +1,3 @@
-export const APP_SOCCER_SERVER_URL = 'https://soccer-app.herokuapp.com/api/v1';
+export const APP_SOCCER_SERVER_URL = 'http://localhost:8000/api/v1';
 
 export const APP_SOCCER_JWT_KEY = 'AppSoccerJWT';
