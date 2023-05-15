9984164c8d03d8f00835f4aa29dd840c4a641d0b
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2023-05-15 13:11:05 -0600

    App Soccer FE | Fix overlapping playoffs keys and navigation and hidding play playoffs matches button

--- src/app/components/play-tournament-dashboard/playoffs/playoffs.component.ts
+++ src/app/components/play-tournament-dashboard/playoffs/playoffs.component.ts
@@ -13,7 +13,8 @@ import { CHAMPION_ALERT } from '../../../../assets/consts/configs/alerts-config.
 })
 export class PlayoffsComponent implements OnInit {
   @Input() PlayoffsTeams$: Observable<IPositionTableData[]>;
-
+  @Input() totalMatches: number;
+  @Input() currentMatch: number;
   PlayoffsCalendar$: Observable<any>;
   currentMatchIndex$ = new BehaviorSubject(0);
   currentPhase$ = new BehaviorSubject(0);
