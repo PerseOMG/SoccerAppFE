58409ba27ce175f292e2c0e950cb6adb691004ba
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2023-07-24 17:21:09 -0600

    App Soccer FE | Reorganize import statements from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { DragulaService, DragulaModule } from 'ng2-dragula';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StringFilterPipe } from './pipes/string-filter.pipe';
 import { HeaderComponent } from './components/shared/navigation/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './components/card/card.component';
 import { CardsContainerComponent } from './components/card/cards-container/cards-container.component';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
 import { reducers } from './services';
-import { EffectsModule } from '@ngrx/effects';
 import { TeamsEffects } from './services/teams/teams.effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { CardDetailComponent } from './components/card/card-detail/card-detail.component';
 import { CardFullComponent } from './components/card/card-full/card-full.component';
 import { FooterComponent } from './components/shared/navigation/footer/footer.component';
 import { CreditsPageComponent } from './components/common/credits-page/credits-page.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { FiltersComponent } from './components/shared/filter/filters/filters.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { StringFilterPipe } from './pipes/string-filter.pipe';
 import { LoginComponent } from './components/auth/login/login.component';
 import { AuthEffects } from './services/auth/auth.effects';
 import { RegisterComponent } from './components/auth/register/register.component';
@@ -31,9 +33,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { CreateButtonComponent } from './components/shared/action-button/action-button.component';
 import { CreateFormComponent } from './components/shared/create/create-form/create-form.component';
-import { NgSelectModule } from '@ng-select/ng-select';
 import { DragAndDropComponent } from './components/shared/create/drag-and-drop/drag-and-drop.component';
-import { DragulaService, DragulaModule } from 'ng2-dragula';
 import { TournamentsEffects } from './services/tournaments/tournaments.effects';
 import { TournamentsCardComponent } from './components/tournaments/tournaments-card/tournaments-card.component';
 import { TournamentDetailsComponent } from './components/tournaments/tournament-details/tournament-details.component';
@@ -42,6 +42,7 @@ import { PositionTableComponent } from './components/play-tournament-dashboard/p
 import { PlayoffsComponent } from './components/play-tournament-dashboard/playoffs/playoffs.component';
 import { CalendarComponent } from './components/play-tournament-dashboard/calendar/calendar.component';
 import { NoDataComponent } from './components/shared/no-data/no-data.component';
+import { FiltersComponent } from './components/shared/filters/filters.component';
 
 @NgModule({
   declarations: [
