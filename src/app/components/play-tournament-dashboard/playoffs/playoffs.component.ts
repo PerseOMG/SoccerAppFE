466d1205c17b7085eb08f75ab5b65d7cfc59735b
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2023-08-09 17:24:12 -0600

    App Soccer FE | Rollback playoffs logic changes

--- src/app/components/play-tournament-dashboard/playoffs/playoffs.component.ts
+++ src/app/components/play-tournament-dashboard/playoffs/playoffs.component.ts
@@ -39,27 +39,33 @@ export class PlayoffsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.PlayoffsCalendar$ = this.PlayoffsTeams$.pipe(
-      map((teams) => {
+    this.PlayoffsCalendar$ = combineLatest([this.PlayoffsTeams$]).pipe(
+      map(([teams]) => {
         const calendar = [];
-        const numTeams = teams.length;
-
-        for (let idx = numTeams; idx >= 2; idx /= 2) {
+        const matches = [];
+        for (let i = 0; i < teams.length / 2; i++) {
+          matches.push({
+            local: teams[i],
+            visit: teams[teams.length - i - 1],
+            localScore: 0,
+            visitScore: 0,
+          });
+        }
+        calendar.push(matches);
+        let idx = 2;
+        while (teams.length / idx !== 1) {
+          idx = idx * 2;
           const matches = [];
-          for (let i = 0; i < numTeams / idx; i++) {
+          for (let i = 0; i < teams.length / idx; i++) {
             matches.push({
-              local: idx === numTeams ? teams[i] : { team: { name: 'NA' } },
-              visit:
-                idx === numTeams
-                  ? teams[numTeams - i - 1]
-                  : { team: { name: 'NA' } },
+              local: { team: { name: 'NA' } },
+              visit: { team: { name: 'NA' } },
               localScore: 0,
               visitScore: 0,
             });
           }
           calendar.push(matches);
         }
-
         return calendar;
       })
     );
