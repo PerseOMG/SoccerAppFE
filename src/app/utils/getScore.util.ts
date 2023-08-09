f7031bc22302eb79d957ab466c93dd8131278a01
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2023-08-09 17:31:18 -0600

    App Soccer FE | Optimize getScore logic to improve readability

--- src/app/utils/getScore.util.ts
+++ src/app/utils/getScore.util.ts
@@ -1,15 +1,21 @@
 export const getScore = () => {
   let scoreLocal = 0;
   let scoreVisit = 0;
+
   for (let i = 0; i < 12; i++) {
     const localProbabilityToScore = Math.random();
     const visitProbabilityToScore = Math.random();
-    if (localProbabilityToScore - visitProbabilityToScore > 0.5) {
+
+    const localScores = localProbabilityToScore - visitProbabilityToScore;
+    const visitScores = visitProbabilityToScore - localProbabilityToScore;
+
+    if (localScores > 0.5) {
       scoreLocal++;
     }
-    if (visitProbabilityToScore - localProbabilityToScore > 0.5) {
+    if (visitScores > 0.5) {
       scoreVisit++;
     }
   }
+
   return { scoreLocal, scoreVisit };
 };
