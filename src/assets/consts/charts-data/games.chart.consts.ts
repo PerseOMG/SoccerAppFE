12a6fcbc7114a59f43c6b196ac5e9c44760ed907
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2023-08-31 15:45:42 -0600

    App Soccer FE | Changing games labels to matches

--- src/assets/consts/charts-data/games.chart.consts.ts
+++ src/assets/consts/charts-data/games.chart.consts.ts
@@ -3,9 +3,9 @@ export const GAMES_CHART_CONFIG = {
 };
 
 export const GAMES_CHART_DATA = {
-  labels: ['Total Games Lost', 'Total Games Won', 'Total Tied Games'],
+  labels: ['Total Matches Lost', 'Total Matches Won', 'Total Tied Matches'],
   datasets: {
-    label: 'Total Games',
+    label: 'Total Matches',
     backgroundColor: ['#273748', '#607eaa', '#f9f5eb'],
     borderColor: ['black'],
     borderWidth: 1,
