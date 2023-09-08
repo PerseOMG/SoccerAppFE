cfc47b1b5e28b49ff3ac88634dd943e2918f9b59
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2023-09-08 16:46:55 -0600

    App Soccer FE | *wip* Creating input to select calendar phase to display

--- src/app/components/play-tournament-dashboard/calendar/calendar.component.ts
+++ src/app/components/play-tournament-dashboard/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-calendar',
@@ -7,7 +8,23 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
   @Input() calendar: any[];
+  selectedPhaseIdx = 1;
+  selectedPhase$: BehaviorSubject<any[]> = new BehaviorSubject([]);
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.selectedPhase$.next(this.calendar[this.selectedPhaseIdx - 1]);
+  }
+
+  checkValue() {
+    if (
+      this.selectedPhaseIdx - 1 >= 0 &&
+      this.selectedPhaseIdx - 1 < this.calendar.length
+    ) {
+      this.selectedPhase$.next(this.calendar[this.selectedPhaseIdx - 1]);
+    } else {
+      this.selectedPhaseIdx = 1;
+      this.selectedPhase$.next(this.calendar[0]);
+    }
+  }
 }
