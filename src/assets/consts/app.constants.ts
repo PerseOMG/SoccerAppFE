9b4af1e95dc639f3ca139667a9c832a8a0b03083
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2023-09-21 16:43:24 -0600

    APP SOCCER FE | Connected to render service

--- src/assets/consts/app.constants.ts
+++ src/assets/consts/app.constants.ts
@@ -1,3 +1,4 @@
-export const APP_SOCCER_SERVER_URL = 'http://localhost:8000/api/v1';
+export const APP_SOCCER_SERVER_URL =
+  'https://soccer-app-server.onrender.com/api/v1';
 
 export const APP_SOCCER_JWT_KEY = 'AppSoccerJWT';
